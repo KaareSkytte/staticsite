9d4ccd10b9bd5f88cfb422f7437f189afda3e9b2
KaareSkytte <Kaareskytte1431@gmail.com>
2025-02-27 15:02:13 +0100

    Added Leafnode + Tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,3 +20,16 @@ class HTMLNode():
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
     
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value):
+        super().__init__(tag, value)
+        self.children = None
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_none(self):
@@ -24,6 +24,39 @@ class TestHTMLNode(unittest.TestCase):
         # Remember order might vary with dictionaries
         self.assertEqual(node.props_to_html(), ' class="button" id="submit"')
 
+
+
+
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_with_tag(self):
+        # Normal case with tag and value
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_no_tag(self):
+        # No tag present
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_with_different_tag(self):
+        # Testing another tag
+        node = LeafNode("a", "Click me")
+        self.assertEqual(node.to_html(), "<a>Click me</a>")
+
+    def test_leaf_to_html_no_value(self):
+        # Should raise ValueError if no value is provided
+        with self.assertRaises(ValueError):
+            node = LeafNode("p", None)
+            node.to_html()
+
+    def test_leaf_to_html_with_props_ignored(self):
+        # If props exist, they should not interfere
+        node = LeafNode("span", "Test")
+        node.props = {"class": "highlight"}  # Set props manually
+        self.assertEqual(node.to_html(), "<span>Test</span>")
+        # Ensure props do not influence LeafNode's to_html behavior
 
 if __name__ == "__main__":
     unittest.main()
